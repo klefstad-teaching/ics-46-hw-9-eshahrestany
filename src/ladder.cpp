97d65e6160b48d45cab4a1656e2d30f2ef0f54f7
Evan Shahrestany <eashahre@uci.edu>
2025-03-13 01:03:09 -0700

    this autograder weird

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -21,8 +21,16 @@ void verify_word_ladder() {
 
 void print_word_ladder(const vector<string>& ladder)
 {
+    if (ladder.empty())
+    {
+        cout << "No word ladder found." << endl;
+        return;
+    }
+
+    cout << "Word ladder found: ";
     for (const string& word : ladder)
         cout << word << " ";
+    cout << endl;
 }
 
 void load_words(set<string> & word_list, const string& file_name)
