c48fb65b2da1cdd68550c13399bc6f9712ef93d3
Evan Shahrestany <eashahre@uci.edu>
2025-03-12 21:47:58 -0700

    misc functions

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,6 +1,11 @@
 #include "ladder.h"
 
+// misc
 
+void error(string word1, string word2, string msg)
+{
+    cout << msg << "; word1: " << word1 << ", word2: " << word2 << endl;
+}
 
 #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 void verify_word_ladder() {
@@ -12,4 +17,15 @@ void verify_word_ladder() {
     my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
     my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
     my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
+
+void print_word_ladder(const vector<string>& ladder)
+{
+    for (const string& word : ladder)
+        cout << word << " ";
+}
+
+int main()
+{
+    return 0;
 }
