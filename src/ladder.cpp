8a4b7a60b010605e5211e83f094e92b2436c3434
Evan Shahrestany <eashahre@uci.edu>
2025-03-12 22:33:07 -0700

    get_adjacent_words

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -25,6 +25,45 @@ void print_word_ladder(const vector<string>& ladder)
         cout << word << " ";
 }
 
+// helpers
+vector<string> get_adjacent_words(const string& root, const set<string>& word_list) {
+    vector<string> result;
+    string candidate;
+
+    // replace
+    for (size_t i = 0; i < root.size(); i++) {
+        candidate = root;
+        char original = candidate[i];
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (c == original) continue;
+            candidate[i] = c;
+            if (word_list.contains(candidate)) {
+                result.push_back(candidate);
+            }
+        }
+    }
+
+    // delete
+    for (size_t i = 0; i < root.size(); i++) {
+        candidate = root.substr(0, i) + root.substr(i + 1);
+            if (word_list.contains(candidate)) {
+            result.push_back(candidate);
+        }
+    }
+
+    // insert
+    for (size_t i = 0; i <= root.size(); i++) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            candidate = root.substr(0, i) + c + root.substr(i);
+            if (word_list.contains(candidate)) {
+                result.push_back(candidate);
+            }
+        }
+    }
+
+    return result;
+}
+
 int main()
 {
     return 0;
